adaec1c61db2fb54fcaab7f443212421d09a624a
Brad McDanel <bmcdanel@fandm.edu>
2022-02-16 13:02:07 -0500

    Socket programming intro

--- udp-example/src/client.cpp
+++ udp-example/src/client.cpp
@@ -3,25 +3,28 @@
 
 using asio::ip::udp;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   asio::io_context io_context;
+  if (argc != 2) {
+    std::cerr << "Usage: client <host>\n";
+    return 1;
+  }
+  auto host = argv[1];
   udp::resolver resolver(io_context);
   udp::endpoint receiver_endpoint =
-      *resolver.resolve(udp::v4(), argv[1], "3000").begin();
+      *resolver.resolve(udp::v4(), host, "3000").begin();
 
   udp::socket socket(io_context);
   socket.open(udp::v4());
-  std::array<uint8_t, 1> empty;
-  std::array<uint8_t, 1> counter = {0};
+  std::array<uint8_t, 1> send;
+  std::array<uint8_t, 1> recv;
 
-  for (int i = 0; i < 256; ++i) {
-    socket.send_to(asio::buffer(empty), receiver_endpoint);
-  }
-
-  for (int i = 0; i < 256; ++i) {
+  for (int i = 0; i < 8; ++i) {
+    send[0] = i;
+    socket.send_to(asio::buffer(send), receiver_endpoint);
     udp::endpoint sender_endpoint;
-    size_t len = socket.receive_from(asio::buffer(counter), sender_endpoint);
-    std::cout << std::to_string(counter[0]) << "  " << i << std::endl;
+    size_t len = socket.receive_from(asio::buffer(recv), sender_endpoint);
+    std::cout << "Sent: " << i << ", Received: " << +recv[0] << std::endl;
   }
 
   return 0;
--- udp-example/src/server.cpp
+++ udp-example/src/server.cpp
@@ -1,6 +1,6 @@
-#include <inttypes.h>
 #include <asio.hpp>
 #include <ctime>
+#include <inttypes.h>
 #include <iostream>
 #include <string>
 
@@ -9,20 +9,16 @@ using asio::ip::udp;
 int main() {
   asio::io_context io_context;
   udp::socket socket(io_context, udp::endpoint(udp::v4(), 3000));
-
   asio::error_code error;
-  std::array<uint8_t, 1> empty;
-  std::array<uint8_t, 1> counter = {0};
+  std::array<uint8_t, 1> send;
+  std::array<uint8_t, 1> recv;
   while (true) {
     udp::endpoint remote_endpoint;
-
-    socket.receive_from(asio::buffer(empty), remote_endpoint);
-    socket.send_to(asio::buffer(counter), remote_endpoint, 0, error);
-    std::cout << std::to_string(counter[0]) << std::endl;
-    if (counter[0] == 255) {
-      break;
-    }
-    counter[0] += 1;
+    socket.receive_from(asio::buffer(recv), remote_endpoint);
+    send[0] = recv[0] + 1;
+    socket.send_to(asio::buffer(send), remote_endpoint, 0, error);
+    std::cout << "Received: " << +recv[0] << ", Sent: " << +send[0]
+              << std::endl;
   }
 
   return 0;
